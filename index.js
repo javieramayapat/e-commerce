d48f7f562177d912acad5e62e424abfaf516894e
Javier Amaya <Javieramayapat@gmail.com>
2022-06-20 12:45:40 -0500

    [feat] Add new routes using JSON response

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/new-route', (req, res) => {
+  res.status(200).send("Hello new route")
+});
+
+
+app.get('/products', (req, res) => {
+  res.status(200).json({
+    "id": 1,
+    "name": "Laptop"
+  });
+});
+
+
+app.get('/categories', (req, res) => {
+  res.status(200).json({
+    "id": 1,
+    "name": "Farmacy",
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listen in port ${port}`)
 });
