f7c04d60f5e1b85a134c79dd0a8f8bafdb6ae70d
Javier Amaya <Javieramayapat@gmail.com>
2022-06-20 13:45:21 -0500

    [feat] Practice how to recive params by the route

--- index.js
+++ index.js
@@ -14,17 +14,92 @@ app.get('/new-route', (req, res) => {
 
 
 app.get('/products', (req, res) => {
-  res.status(200).json({
-    "id": 1,
-    "name": "Laptop"
-  });
+  res.status(200).json([
+    {
+      "id": 1,
+      "name": "t-shirts",
+      "price": 150.60
+    },
+    {
+      "id": 2,
+      "name": "pants",
+      "price": 500
+    },
+    {
+      "id": 3,
+      "name": "pants",
+      "price": 200
+    }
+  ]);
 });
 
 
 app.get('/categories', (req, res) => {
+  res.status(200).json([
+    {
+      "id": 1,
+      "name": "Farmacy",
+    },
+    {
+      "id": 2,
+      "name": "Clotes",
+    }
+  ]);
+});
+
+
+// Chalenge: create a route to see the detail of a product
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.status(200).json({
-    "id": 1,
-    "name": "Farmacy",
+    "id": id,
+    "name": "t-shirts",
+    "price": 150.60
+  });
+});
+// Chalenge: create a route to see the detail of a cataegory
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({
+    "id": id,
+    "name": "pants",
+    "price": 500
+
+  })
+});
+// Chalenge: create a route which recive multiple params
+// example: Get from a special categgory an specific product
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.status(200).json({
+    "categoryId": categoryId,
+    "productId": productId
+  });
+});
+
+
+app.get('/people', (req, res) => {
+  res.status(200).json([
+    {
+      "id": 1,
+      "name": "Juan Carlos",
+      "edad": 26
+    },
+    {
+      "id": 2,
+      "name": "Liliana Amaya",
+      "edad": 18
+    }
+  ]);
+});
+
+app.get('/people/:id/', (req, res) => {
+  const { id } = req.params;
+
+  res.status(200).json({
+    "id": id,
+    "name": "Elvira Guzman",
+    "edad": 33
   });
 });
 
