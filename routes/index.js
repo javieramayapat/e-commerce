66d29dedcb8f5a007c8d5aa23a965323c8a0adb5
Javier Amaya <Javieramayapat@gmail.com>
2022-06-20 23:42:46 -0500

    [feat] Add master router to add versioning layer to my API routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const express = require('express')
+
 const productsRouter = require('./products.router.js')
 const categoriesRouter = require('./categories.router.js')
 const usersRouter = require('./users.router.js')
@@ -5,12 +7,25 @@ const usersRouter = require('./users.router.js')
 /**
  * Index.js is going to be the encharger to get the app from the entrypoint and
  * pass it to the routes to enable the functionalities.
+ *
+ * Challenge: Create a master router that allow integrate the suffixes required for api versioning
+ * from this format:
+ * ❌app.use('/api/v1/products', productsRouter);
+ * ❌app.use('/api/v1/categories', categoriesRouter);
+ *
+ * To this format:
+ * app.user('/api/v1', router)
+ * ✅route.use('/products', productsRouter);
  */
 
 function routerApi(app) {
-  app.use('/api/v1/products', productsRouter);
-  app.use('/api/v1/categories', categoriesRouter);
-  app.use('/api/v1/users', usersRouter);
+  // Creating a master route to add a versioning layer to my api routes
+  const router = express.Router();
+  app.use('/api/v1', router)
+
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
 
 
