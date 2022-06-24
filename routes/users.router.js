f78f404e58c4d883205997ef44b90537edc2df1f
Javier Amaya <Javieramayapat@gmail.com>
2022-06-24 00:16:48 -0500

    [feat] Add post, put,patch and delete user routes

--- routes/users.router.js
+++ routes/users.router.js
@@ -4,22 +4,25 @@ const express = require('express')
 const router = express.Router();
 
 
+
+
 // Challenge: recive query parameter limit, offset
 // Example: http://localhost:3000/users?limit=54&offset=41
 router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-
-  if (!limit && !offset) {
-    res.json({
-      "message": "No limit and offset recived"
-    });
-  }
-
-  res.status(200).json({
-    "limit": limit,
-    "offset": offset
-  })
-})
+  res.status(200).json([
+    {
+      id: 1,
+      name: "Javier",
+      email: "javier@gmail.com"
+    },
+    {
+      id: 1,
+      name: "Daniela",
+      email: "Daniela@gmail.com",
+      example: example
+    }
+  ]);
+});
 
 
 router.get('/:id', (req, res) => {
@@ -30,6 +33,51 @@ router.get('/:id', (req, res) => {
     "name": "Elvira Guzman",
     "edad": 33
   });
+});
+
+
+router.post('/', (req, res) => {
+  const body = req.body;
+
+  res.status(201).json({
+    message: "created",
+    body: body
+  });
+
+});
+
+
+router.put('/:id', (req, res) => {
+
+  const { id } = req.params;
+  const body = req.body;
+
+  res.status(200).json({
+    message: "Update user",
+    id: id,
+    body: body
+  });
+});
+
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  res.status(200).json({
+    message: "Partial update",
+    id: id,
+    body: body
+  });
+});
+
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(204).json({
+    message: "Delete",
+    id: id
+  })
 });
 
 module.exports = router
